fd5066c06d9bd672e0920fa20664ae0b300b0aa8
kartikkandpal <kandpalkartik.13@gmail.com>
2025-06-12 00:11:17 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,80 @@
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+
+let started = false;
+let level = 0;
+
+const clickSound = new Audio("click.mp3");
+const wrongSound = new Audio("wrong.mp3");
+
+document.addEventListener("keydown", () => {
+  if (!started) {
+    document.getElementById("level-title").textContent = "Level " + level;
+    nextSequence();
+    started = true;
+  }
+});
+
+document.querySelectorAll(".btn").forEach(button => {
+  button.addEventListener("click", function () {
+    const userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+  });
+});
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  document.getElementById("level-title").textContent = "Level " + level;
+
+  const randomNumber = Math.floor(Math.random() * 4);
+  const randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  const button = document.getElementById(randomChosenColor);
+  button.classList.add("pressed");
+  playSound(randomChosenColor);
+  setTimeout(() => button.classList.remove("pressed"), 300);
+}
+
+function playSound(name) {
+  if (name === "wrong") {
+    wrongSound.play();
+  } else {
+    clickSound.play();
+  }
+}
+
+function animatePress(currentColor) {
+  const button = document.getElementById(currentColor);
+  button.classList.add("pressed");
+  setTimeout(() => button.classList.remove("pressed"), 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(nextSequence, 1000);
+    }
+  } else {
+    playSound("wrong");
+    document.body.classList.add("game-over");
+    document.getElementById("level-title").textContent = "Game Over, Press Any Key to Restart";
+
+    setTimeout(() => {
+      document.body.classList.remove("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
